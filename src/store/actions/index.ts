693b9f2d496ed374fa9c9fbf2ccc9cdb9e267724
Aleksey K <aleksey.kutovoy@so4it.com>
2021-08-28 20:25:42 +0300

    Minor cleanup.

--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -1,21 +1,5 @@
 import { BaseAction, actionIds, MarketingSearchQuery } from 'store/common';
 
-export const numberRequestStartAction = (): BaseAction => ({
-  type: actionIds.GET_NUMBER_REQUEST_START,
-  payload: null,
-});
-
-
-
-export const numberRequestCompletedAction = (
-  numberGenerated: number
-): BaseAction => ({
-  type: actionIds.GET_NUMBER_REQUEST_COMPLETED,
-  payload: numberGenerated,
-});
-
-/* ================================================================= */
-
 export const getMarketingData = (): BaseAction => ({
   type: actionIds.GET_ALL_MARKETING_DATA_REQUESTED,
   payload: null,
